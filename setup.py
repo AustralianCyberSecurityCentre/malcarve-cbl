d7322be57be53bfcc66ba6c814e6c90db972a689
dan-acsc <130635354+dan-acsc@users.noreply.github.com>
2025-12-19 08:23:12 +1100

    feat: prep for pypi release. (#6)
    feat: prep for pypi release.
    
    Co-authored-by: dan-acsc <dan-acsc@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -17,6 +17,8 @@ class CBuild(build):
         subprocess.run(build_script_path, check=True)
         build.run(self)
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 def open_file(fname):
     """Open and return a file-like object for the relative filename."""
@@ -33,7 +35,19 @@ setup(
     package_data={"malcarve_cbl": ["ext/build/keyedDecrypt.so"]},
     include_package_data=True,
     python_requires=">=3.12",
-    classifiers=[],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+    ],
     use_scm_version=True,
     setup_requires=["setuptools_scm"],
     install_requires=[r.strip() for r in open_file("requirements.txt") if not r.startswith("#")],
